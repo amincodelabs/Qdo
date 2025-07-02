c3675317b99391c04ae1a3159875aca4316887a8
Amin Chahardoli <chahardoli76@gmail.com>
2025-07-02 23:08:08 +0400

    feat: create data, domain and infra layer for task.

---
+++ app/src/main/java/amin/codelabs/qdo/data/task/TaskEntity.kt
@@ -0,0 +1,13 @@
+package amin.codelabs.qdo.data.task
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class TaskEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0L,
+    val title: String,
+    val description: String? = null,
+    val isDone: Boolean = false,
+    val createdAt: Long = System.currentTimeMillis()
+) 
