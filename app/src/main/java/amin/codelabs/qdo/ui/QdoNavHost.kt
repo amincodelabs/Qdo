db66b824e52860e22155f9c5f34232c6e0dfd839
AminCodeLabs <aristhewonder@gmail.com>
2025-07-06 14:09:12 +0400

    fix: task deletion message.

--- app/src/main/java/amin/codelabs/qdo/ui/QdoNavHost.kt
+++ app/src/main/java/amin/codelabs/qdo/ui/QdoNavHost.kt
@@ -1,11 +1,14 @@
 package amin.codelabs.qdo.ui
 
+import amin.codelabs.qdo.feature.tasklist.TaskListViewModel
+import amin.codelabs.qdo.feature.tasklist.contract.TaskListIntent
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import amin.codelabs.qdo.ui.navigation.taskListNavGraph
 import amin.codelabs.qdo.ui.navigation.addTaskNavGraph
 import amin.codelabs.qdo.ui.navigation.taskDetailsNavGraph
+import androidx.hilt.navigation.compose.hiltViewModel
 
 sealed class QdoDestination(val route: String) {
     data object TaskList : QdoDestination("tasklist")
@@ -16,13 +19,16 @@ sealed class QdoDestination(val route: String) {
 }
 
 @Composable
-fun QdoNavHost(navController: NavHostController) {
+fun QdoNavHost(navController: NavHostController, taskListViewModel: TaskListViewModel = hiltViewModel()) {
     NavHost(
         navController = navController,
         startDestination = QdoDestination.TaskList.route
     ) {
-        taskListNavGraph(navController)
+        taskListNavGraph(navController, taskListViewModel)
         addTaskNavGraph(navController)
-        taskDetailsNavGraph(navController)
+        taskDetailsNavGraph(navController, onTaskDeleted = {
+            taskListViewModel.processIntent(TaskListIntent.TaskDeleted)
+        })
     }
-} 
+}
+
